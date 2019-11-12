57253190cb8ae1e1065a4131b5eafc8c0be6331c
stadnikivan <stadnik11ivan@gmail.com>
2019-11-12 17:59:10 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+//1
+function pow(x, n) {
+var a = (10, false, "google");
+  if (n == 1) {
+    return x;
+  } else {
+    return x * pow(x, n - 1), a;
+  }
+}
+//2
+function userInfo() {
+	var registered = "Дата регистрации: ",
+		date = new Date,
+		str = "Незарегистрированный пользователь: ";
+		if (registered = false) {
+			date = undefined;
+			return(alert(str)+name)
+		}
+	let user1 = {
+		name: "John",
+		register: 2 == 1,
+		d: date,
+		getInfo: globalThis,
+	};
+	let user2 = {
+		name: "Sergey",
+		register: 2 != 1,
+		d: date,
+		getInfo: globalThis,
+	};
+}
+userInfo.user1;
+userInfo.user2;
+//3
+
+
+
+
+
+
+
+
+var arr = [];
+for ( i = 0; i < 7; i++ ) {
+    arr.push( Math.round( Math.random() * 100 ));
+if (50 < arr[i])
+    alert("i = "+arr[i]);
+} console.log(arr);
